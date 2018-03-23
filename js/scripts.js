dee9012ed0bc6a3e0978043de3b0b8cfdf63dc51
Pablo Barreyro <pabarreyro@gmail.com>
2018-03-23 08:23:16 -0700

    add survey scoring loop to frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 var countA = function(string){
   return string.count("a");
 };
-
 var countB = function(string){
   return string.count("b");
 };
-
 var countC = function(string){
   return string.count("c");
 };
 
 $(document).ready(function() {
+  $("#bootcampSurvey").submit(function(event) {
+    var inputs = ["q1", "q2", "q3", "q4", "q5"];
+    var userScore = "";
 
+    inputs.forEach(function(input) {
+      userScore += $("#" + input).val();
+    });
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
 });
