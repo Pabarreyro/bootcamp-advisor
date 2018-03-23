4aff5be446abb165fda55842bd47a6d37ae2c1e5
Pablo Barreyro <pabarreyro@gmail.com>
2018-03-23 08:11:49 -0700

    add string counter into backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var countA = function(string){
+  return string.count("a");
+};
+
+var countB = function(string){
+  return string.count("b");
+};
+
+var countC = function(string){
+  return string.count("c");
+};
+
 $(document).ready(function() {
 
 });
