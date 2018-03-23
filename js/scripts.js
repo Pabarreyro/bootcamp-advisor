1e37cc0375be4a41055d6c55da834b7a6164a679
Pablo Barreyro <Pabarreyro@gmail.com>
2018-03-23 14:18:44 -0700

    add branching to interface logic in order to handle survey results output

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,38 @@ $(document).ready(function() {
     frequencies.push(countA(userScore), countB(userScore), countC(userScore));
     console.log(frequencies);
 
-    $("#output").slideToggle("slow");
+    if (frequencies[0] >= 3) {
+      $("#course_name").text(courseNames[0]);
+      $("#course_info").text(courseInfo[0]);
+      $("#output").slideToggle("slow");
+    } else if (frequencies[1] >= 3) {
+      $("#course_name").text(courseNames[1]);
+      $("#course_info").text(courseInfo[1]);
+      $("#output").slideToggle("slow");
+    } else if (frequencies[2] >= 3) {
+      $("#course_name").text(courseNames[2]);
+      $("#course_info").text(courseInfo[2]);
+      $("#output").slideToggle("slow");
+    } else {
+      if (frequencies[0] === frequencies[1]) {
+        $("#course_name").text(courseNames[0]);
+        $("#course_info").text(courseInfo[0]);
+        $("#course_name2").text(courseNames[1]);
+        $("#course_info2").text(courseInfo[1]);
+
+        $("#output").slideToggle("slow");
+        $("#output2").slideToggle("slow");
+      } else {
+        $("#course_name").text(courseNames[1]);
+        $("#course_info").text(courseInfo[1]);
+        $("#course_name2").text(courseNames[2]);
+        $("#course_info2").text(courseInfo[2]);
+
+        $("#output").slideToggle("slow");
+        $("#output2").slideToggle("slow");
+      }
+    }
+
 
     event.preventDefault();
   });
